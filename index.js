9768679e51e66953de4058e007adf5082a1b0167
Tiger14n <nasser11r1@gmail.com>
2021-08-08 23:33:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -314,19 +314,32 @@ function getHelpString() {
         out += '```'
     return out;
 }
-discordClient.on("ready", () => {
+
+async function connect(msg, mapKey) {
+    discordClient.on("ready", () => {
     const channel = discordClient.channels.fetch(msg.member.voice.channelID);
     if (!channel) return console.error("The channel does not exist!");
     channel.join().then(connection => {
         // Yay, it worked!
         console.log("Successfully connected.");
+           voice_Connection.play('sound.mp3', { volume: 0.5 });
+        guildMap.set(mapKey, {
+            'text_Channel': text_Channel,
+            'voice_Channel': voice_Channel,
+            'voice_Connection': voice_Connection,
+            'musicQueue': [],
+            'musicDispatcher': null,
+            'musicYTStream': null,
+            'currentPlayingTitle': null,
+            'currentPlayingQuery': null,
+            'debug': false,
+        });
     }).catch(e => {
 
         // Oh no, it errored! Let's log it to console :)
         console.error(e);
     });
 });
-async function connect(msg, mapKey) {
     try {
         let voice_Channel = await discordClient.channels.fetch(msg.member.voice.channelID);
         if (!voice_Channel) return msg.reply("Error: The voice channel does not exist!");
